6380311d11957be4ca67ccda3a3773b5eca8e46a
max <>
2026-08-30 03:47:13 -0700

    KB-336 deploy v1.0.52: cross-tab shared-token deactivation fix

--- onion-transport/wasm-status-pkg/wasm_status_bg.wasm.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const ping: (a: number) => number;
-export const wasm_ready: () => number;
-export const __wbindgen_externrefs: WebAssembly.Table;
-export const __wbindgen_start: () => void;
