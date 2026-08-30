dbcf2269d12e1e2c33613f68cb0c0a8148bc99f0
max <>
2026-08-30 03:51:02 -0700

    KB-336 deploy fixup: restore auth/onion-transport/.nojekyll/gateway-tunnel.json wiped by wholesale dist copy

---
+++ onion-transport/wasm-status-pkg/wasm_status_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const ping: (a: number) => number;
+export const wasm_ready: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
