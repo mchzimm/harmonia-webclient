944df614c9915a07e7a211c2a0c8c10c21fc1440
max <>
2026-09-04 13:40:30 -0700

    deploy: v1.0.136 -- KB-468 correction: max-merge migration, falsification test, corrected root-cause docs

--- harmonia-ui-e01be4121e5dc342.js
+++ harmonia-ui-e01be4121e5dc342.js
@@ -1381,7 +1381,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000013: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("RTCDataChannelEvent")], shim_idx: 1426, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("RTCDataChannelEvent")], shim_idx: 1443, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h8f5765081a796a8b);
             return ret;
         },
