5127a2b5793e96ac7b90760590c169785f89421d
max <>
2026-09-04 14:20:23 -0700

    deploy: v1.0.138 -- fix data-loss bug in KB-468 migration (fp_key==legacy_key deletes marks)

--- harmonia-ui-aeb53526176be6f0.js
+++ harmonia-ui-aeb53526176be6f0.js
@@ -1381,7 +1381,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000013: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("RTCDataChannelEvent")], shim_idx: 1443, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("RTCDataChannelEvent")], shim_idx: 1469, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h0425ae30b75f0cfb);
             return ret;
         },
